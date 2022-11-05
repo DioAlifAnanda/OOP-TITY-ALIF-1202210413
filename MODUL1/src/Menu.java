47fb93c12b7a1ed5a28388cd4be456bca311567b
DioAlifAnanda <dioalif1188@gmail.com>
2022-11-05 08:05:40 +0700

    Modul1

--- MODUL1/src/Menu.java
+++ MODUL1/src/Menu.java
@@ -1,8 +1,28 @@
 public class Menu {
-    public class Menu {
         public String[] nama = {"Bakso","es teh","brownies"};
         public String[] categories = {"(makanan)","(minuman)","(dessert)"};
         public String[] price = {"10000","5000","15000"};
 
-        
+        public void tampilMenu(){
+            System.out.println("1"+ nama[0]+categories[0]+" : "+price[0]);
+            System.out.println("2"+ nama[1]+categories[1]+" : "+price[1]);
+            System.out.println("3"+ nama[2]+categories[2]+" : "+price[2]);
+
+    }
+    public void detailMenu(String a){
+        switch(a){
+            case "Bakso":
+            System.out.println(nama[0]+categories[0]+" : "+price[0]);
+            break;
+            case "es teh":
+            System.out.println(nama[1]+categories[1]+" : "+price[1]);
+            break;
+            case " brownies":
+            System.out.println(nama[2]+categories[2]+" : "+price[2]);
+            break;
+            default:
+            System.out.println("Tidak ada menu pilihan ");
+            break;
+        }   
+    } 
 }
